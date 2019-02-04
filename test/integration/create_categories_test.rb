49deac532cc0576d687b0d5a871ad6fc46384b85
= <=>
2019-02-04 17:54:01 +0500

    Fixed tests and updated categories section

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,10 +1,14 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.create(username: "Ali", email:"john@example.com", password:"password", admin:true)
+  end
   test 'get new category form and and category' do
-  get new_category_path
-  assert_template 'categories/new'
-  assert_difference 'Category.count', 1 do
+    sign_in_as(@user, "password")
+    get new_category_path
+    assert_template 'categories/new'
+    assert_difference 'Category.count', 1 do
     post categories_path, params:{category: {name:"sports"}}
     follow_redirect! 
   end
@@ -13,7 +17,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
 end
 
 test "Invalid get new category" do
- 
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
