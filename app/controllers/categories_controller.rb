7c510a55297dffb417e34ede35272daa9a1d97e1
= <=>
2019-02-11 12:52:34 +0500

    Added form functionality for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,6 +7,8 @@ before_action :require_admin, except:[:index, :show]
     @category = Category.new
   end
   def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page:5)
   end
   def create
     @category = Category.new(category_params)
